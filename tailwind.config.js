77f4993beb494291885e0df9340fc7d7e7cf38c9
Hopeson <hopesonogaga@gmail.com>
2024-01-26 17:12:11 +0100

    ui timer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         roboto: "Roboto, sans-serif",
       },
+
+      colors: {
+        "Dark-desaturated-blue": "hsl(236, 21%, 26%)",
+        "Very-dark-blue": " hsl(235, 16%, 14%)",
+        "Very-dark-mostly-black": " hsl(234, 17%, 12%)",
+        "Soft-red": "hsl(345, 95%, 68%)",
+        " Grayish-blue": "hsl(237, 18%, 59%)",
+        " White": "hsl(0, 0%, 100%)",
+      },
     },
   },
 
